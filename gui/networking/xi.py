b4b207f989b4b63bec1efc613498722cd9d3905d
Venkat Garapati <venkat_garapati@outlook.com>
2019-03-27 12:43:47 -0400

    Update XIPacket Definitions
    Makes a slight adjustment to the packet header definiton.
    
    Also completes the implementation of the XIPacket and Header.
    
    The XIPacket/Header can now be represented by a string.
    
    A XIPacket/Header can now be constructed by a given string buffer.
    
    Adds sequence number to packet header definition.
      Update is as follows:
        XIPacketHeader:
          Second Byte:
      	type          : 2b
    	src           : 1b
    	dst           : 1b
    	seq           : 2b
    	payload (MSB) : 2b
    	-------------------------
    	                8b
    
    Adds test cases to check all possible non default args (with the
    exception of payload) for XIPacket and XIPacketHeader.

--- gui/networking/xi.py
+++ gui/networking/xi.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class PACKET_TYPE(object):
     """
@@ -8,6 +10,7 @@ class PACKET_TYPE(object):
     ACK = 2
     DATA = 3
 
+
 class XIPacketHeader(object):
     """
     Packet Header 
@@ -15,11 +18,18 @@ class XIPacketHeader(object):
     Definition
     -----------
 
-    HEADER (1B) - 0x01 | TYPE (2 bits) | Payload Length (14 bits) | CRC (8 bits)
+    HEADER (1B) - 0x01 | TYPE (2 bits) 
+        | SRC (1 bits) | DST (1 bits) | SEQ NUM (2 bits)
+        | Payload Length (10 bits) | CRC (8 bits)
 
     """
 
-    def __init__(self, type=PACKET_TYPE.DATA, payloadLength=0):
+    def __init__(self,
+                 type=PACKET_TYPE.DATA,
+                 payloadLength=0,
+                 src=0,
+                 dst=0,
+                 seqNum=0):
         """
         Initialize Packet Header.
 
@@ -28,38 +38,126 @@ class XIPacketHeader(object):
                 type of packet
             payloadLength: type=int
                 length of packet payload
+            src: type=int
+                source of the message
+            dst: type=int
+                destination of the message
         """
-        self.header = 0x01 # 1 byte
+        self.header = 0x01  # 1 byte
         self._header_bit_length = 8
-        self.type = type # 2 bits
-        self._type_bit_length = 2
-        self.payloadLength = payloadLength # 14 bits
-        self._payload_length_bit_length = 14
 
-        self.crc = 0 # this will be updated by updateCRC call
+        self.type = type  # 2 bits
+        self._type_bit_length = 2
+
+        self.src = src  # 1 bits
+        self._src_bit_length = 1
+        self.dst = dst  # 1 bits
+        self._dst_bit_length = 1
+
+        self.seqNum = seqNum
+        self._seq_num_bit_length = 2
+
+        self.payloadLength = payloadLength  # 10 bits
+        self._payload_length_bit_length = 10
+
+        self.crc = 0  # this will be updated by updateCRC call
         self._crc_bit_length = 8
         self.updateCRC()
-    
 
     def _get_length(self):
         '''
         Gets length in bytes
         '''
-        length_in_bits = self._header_bit_length + self._crc_bit_length \
-                        + self._payload_length_bit_length + self._type_bit_length
+        length_in_bits = self._header_bit_length + self._type_bit_length \
+                        + self._src_bit_length + self._dst_bit_length \
+                        + self._seq_num_bit_length \
+                        + self._payload_length_bit_length + self._crc_bit_length
 
         return length_in_bits / 8
 
     length = property(_get_length)
 
-
     def updateCRC(self):
-        self.crc = 0xff # crc is set to 0xff to prior to calculation
+        self.crc = 0xff  # crc is set to 0xff to prior to calculation
 
-        self.crc += self.type + self.payloadLength # sum contents of packet header
+        # sum contents of packet header
+        self.crc += self.type
+        self.crc += self.payloadLength
+        self.crc += self.src
+        self.crc += self.dst
+        self.crc += self.seqNum
 
-        self.crc = ~(self.crc % 0xFF) # fit the crc in a byte then perform one's complement
+        # fit the crc in a byte then perform one's complement
+        self.crc = ~(self.crc % 0xFF) % 0xFF
 
+    def _get_rep(self):
+        firstByte = chr(self.header)
+        secondByte = 0
+        thirdByte = 0
+        fourthByte = chr(self.crc)
+
+        # second byte:
+        # [type (2b) | src (1b) | dst (1b) | seq (2b) | MSB payload length (2b)]
+        secondByte = chr(((self.type & 0b11) << 6) + (
+            (self.src & 0b1) << 5) + ((self.dst & 0b1) << 4) + (
+                (self.seqNum & 0b11) << 2) + (
+                    (self.payloadLength & 0x300) >> 8))
+        # third byte:
+        # [LSB payload length (8b)]
+        thirdByte = chr(self.payloadLength & 0xFF)
+
+        return "".join([firstByte, secondByte, thirdByte, fourthByte])
+
+    @staticmethod
+    def createXIPacketHeader(buffer):
+        """
+        Creates a XI Packet Header using a buffer that begins
+        with a valid XIPacketHeader.
+
+        params:
+            buffer: type=str
+
+        returns:
+            None if buffer does not contain a
+            valid XIPacketHeader.
+            Or returns a valid XIPacketHeader.
+        """
+        if len(buffer) < 4:
+            return None
+
+        header = XIPacketHeader()
+        firstByte = ord(buffer[0])
+        secondByte = ord(buffer[1])
+        thirdByte = ord(buffer[2])
+        fourthByte = ord(buffer[3])
+
+        # second byte:
+        # [type (2b) | src (1b) | dst (1b) | seq (2b) | MSB payload length (2b)]
+        _type = (secondByte >> 6) & 0b11
+        src = (secondByte >> 5) & 0b1
+        dst = (secondByte >> 4) & 0b1
+        seqNum = (secondByte >> 2) & 0b11
+        payloadLength = (secondByte & 0b11) << 8
+
+        # third byte:
+        # [LSB payload length (8b)]
+        payloadLength += thirdByte
+
+        crc = fourthByte
+
+        header.type = _type
+        header.src = src
+        header.dst = dst
+        header.seqNum = seqNum
+        header.payloadLength = payloadLength
+        header.updateCRC()
+
+        if firstByte != 0x1 or header.crc != crc:
+            header = None
+
+        return header
+
+    rep = property(_get_rep)
 
 
 class XIPacket(object):
@@ -71,30 +169,106 @@ class XIPacket(object):
 
     Using XIPacketHeader:
 
-    Packet Header (3B) | Payload (Max 16KB) | CRC (1B) | Packet Footer 0x13 (1B)
+    Packet Header (4B) | Payload (Max 1KB-1B) | CRC (1B) | Packet Footer 0x13 (1B)
 
     """
-    def __init__(self, buffer=[]):
 
-        self.header = XIPacketHeader(payloadLength=len(buffer))
-        self.payload = bytearray(buffer) # create a buffer of bytes for the payload
+    def __init__(self, buffer="", type=PACKET_TYPE.DATA, seqNum=0):
+
+        self.header = XIPacketHeader(
+            payloadLength=len(buffer), type=type, seqNum=seqNum)
+        self.payload = buffer
         self.crc = 0
+        self.footer = 0x13
 
         self.updateCRC()
-    
+
     def updateCRC(self):
         self.crc = 0xff
 
         # sum all bytes
         for b in self.payload:
-            self.crc += int(b)
-        
-        self.crc = ~(self.crc % 0xFF) # fit the crc in a byte then perform one's complement
+            if isinstance(b, str):
+                self.crc += ord(b)
+
+        # fit the crc in a byte then perform one's complement
+        self.crc = ~(self.crc % 0xFF) % 0xFF
 
     def _get_length(self):
         # 2 bytes at the end, 1 for the CRC and 1 for 0x13 packet footer
-        return self.header.length + self.header.payloadLength + 2 
+        return self.header.length + self.header.payloadLength + 2
 
     length = property(_get_length)
 
-    
+    def _get_rep(self):
+        rep = []
+        rep.append(self.header.rep)
+        rep.extend(self.payload)
+        rep.append(chr(self.crc))
+        rep.append(chr(self.footer))
+
+        return "".join(rep)
+
+    rep = property(_get_rep)
+
+    def __eq__(self, other):
+        strRep = None
+        if isinstance(other, str):
+            strRep = other
+        elif isinstance(other, XIPacket):
+            strRep = other.rep
+        return self.rep == strRep
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __hash__(self):
+        return hash(self.rep)
+
+
+    @staticmethod
+    def createXIPacket(buffer):
+        """
+        Creates a XI Packet using a buffer that begins
+        with a valid XIPacketHeader.
+
+        params:
+            buffer: type=str
+
+        returns:
+            None if buffer does not contain a
+            valid XIPacket.
+            Or returns a valid XIPacket.
+        """
+        packet = None
+        header = XIPacketHeader.createXIPacketHeader(buffer)
+
+        if header != None:
+            payloadOffset = header.length
+            payloadLength = header.payloadLength
+            crcOffset = payloadOffset + payloadLength
+            footerOffset = crcOffset + 1
+
+            # the expected size is the header + payload + crc + footer
+            expectedSize = header.length + payloadLength + 2
+
+            # buffer must be at least the expected size
+            # as it may contain noise after footer.
+            if len(buffer) >= expectedSize:
+                packet = XIPacket()
+                packet.header = header
+                packet.payload = buffer[payloadOffset:payloadOffset +
+                                        payloadLength]
+                crc = ord(buffer[crcOffset:crcOffset + 1])
+                packet.footer = ord(buffer[footerOffset:footerOffset + 1])
+
+                packet.updateCRC()
+
+                if crc != packet.crc or packet.footer != 0x13:
+                    packet = None
+
+        return packet
+
+    @staticmethod
+    def maxPayload():
+        return 2**(XIPacketHeader()._payload_length_bit_length) - 1
--- gui/networking/xi_test.py
+++ gui/networking/xi_test.py
@@ -1,8 +1,8 @@
 import unittest
 import xi
 
-class TestXIPacketHeader(unittest.TestCase):
 
+class TestXIPacketHeader(unittest.TestCase):
     def test_init(self):
         self.assertNotEqual(xi.XIPacketHeader(), None)
 
@@ -22,26 +22,87 @@ class TestXIPacketHeader(unittest.TestCase):
         header1 = xi.XIPacketHeader(type=xi.PACKET_TYPE.DATA)
         header2 = xi.XIPacketHeader(type=xi.PACKET_TYPE.DATA)
         self.assertEqual(header1.crc, header2.crc)
-    
+
     def test_crc_different_headers(self):
         header1 = xi.XIPacketHeader(type=xi.PACKET_TYPE.ACK, payloadLength=0)
-        header2 = xi.XIPacketHeader(type=xi.PACKET_TYPE.CONNECTION, payloadLength=0)
+        header2 = xi.XIPacketHeader(
+            type=xi.PACKET_TYPE.CONNECTION, payloadLength=0)
         header3 = xi.XIPacketHeader(type=xi.PACKET_TYPE.DATA, payloadLength=0)
 
         self.assertNotEqual(header1.crc, header2.crc)
         self.assertNotEqual(header1.crc, header3.crc)
         self.assertNotEqual(header2.crc, header3.crc)
 
-        header1 = xi.XIPacketHeader(type=xi.PACKET_TYPE.CONNECTION, payloadLength=300)
-        header2 = xi.XIPacketHeader(type=xi.PACKET_TYPE.CONNECTION, payloadLength=3)
-        header3 = xi.XIPacketHeader(type=xi.PACKET_TYPE.CONNECTION, payloadLength=2)
+        header1 = xi.XIPacketHeader(
+            type=xi.PACKET_TYPE.CONNECTION, payloadLength=300)
+        header2 = xi.XIPacketHeader(
+            type=xi.PACKET_TYPE.CONNECTION, payloadLength=3)
+        header3 = xi.XIPacketHeader(
+            type=xi.PACKET_TYPE.CONNECTION, payloadLength=2)
 
         self.assertNotEqual(header1.crc, header2.crc)
         self.assertNotEqual(header1.crc, header3.crc)
         self.assertNotEqual(header2.crc, header3.crc)
 
-class TestXIPacket(unittest.TestCase):
+    def test_create_header(self):
+        # tests the .rep property to ensure that
+        # a new XIPacketHeader can be created by
+        # the string rep of an existing header.
+        header1 = xi.XIPacketHeader(type=xi.PACKET_TYPE.ACK, payloadLength=0)
+        header2 = xi.XIPacketHeader.createXIPacketHeader(header1.rep)
 
+        self.assertEqual(header1.rep, header2.rep)
+
+        header1 = xi.XIPacketHeader(
+            type=xi.PACKET_TYPE.DATA, payloadLength=xi.XIPacket.maxPayload())
+        header2 = xi.XIPacketHeader.createXIPacketHeader(header1.rep)
+
+        self.assertEqual(header1.rep, header2.rep)
+
+        header1 = xi.XIPacketHeader(
+            type=xi.PACKET_TYPE.CONNECTION, payloadLength=2**2)
+        header2 = xi.XIPacketHeader.createXIPacketHeader(header1.rep)
+
+        self.assertEqual(header1.rep, header2.rep)
+
+    def test_bad_header(self):
+        # tests the .rep property to ensure that
+        # a new XIPacketHeader can be created by
+        # the string rep of an existing header.
+        badHeader = ""
+        header = xi.XIPacketHeader.createXIPacketHeader(badHeader)
+
+        badHeader = "".join([chr(0), chr(0), chr(0), chr(0)])
+        header = xi.XIPacketHeader.createXIPacketHeader(badHeader)
+
+        self.assertIsNone(header)
+
+    def test_non_default_values(self):
+        srcBits = xi.XIPacketHeader()._src_bit_length
+        dstBits = xi.XIPacketHeader()._dst_bit_length
+        seqNumBits = xi.XIPacketHeader()._seq_num_bit_length
+
+        # test all possible type, src, dst, seq num combinations
+        for _type in [
+                xi.PACKET_TYPE.ACK, xi.PACKET_TYPE.CONNECTION,
+                xi.PACKET_TYPE.DATA
+        ]:
+            for src in range(2**srcBits):
+                for dst in range(2**dstBits):
+                    for seqNum in range(2**seqNumBits):
+                        header1 = xi.XIPacketHeader(
+                            type=_type,
+                            payloadLength=0,
+                            seqNum=seqNum,
+                            src=src,
+                            dst=dst)
+                        header2 = xi.XIPacketHeader.createXIPacketHeader(
+                            header1.rep)
+
+                        self.assertEqual(header1.rep, header2.rep)
+
+
+class TestXIPacket(unittest.TestCase):
     def test_init(self):
         self.assertNotEqual(xi.XIPacket(), None)
 
@@ -58,33 +119,91 @@ class TestXIPacket(unittest.TestCase):
 
     def test_packet_w_valid_buffer(self):
         payloadLength = 1000
-        packet1 = xi.XIPacket(buffer=[x % 0xFF for x in range(payloadLength)])
+        packet1 = xi.XIPacket(
+            buffer="".join([chr(x % 0xFF) for x in range(payloadLength)]))
 
         self.assertEqual(len(packet1.payload), payloadLength)
         self.assertEqual(packet1.header.payloadLength, payloadLength)
-        self.assertGreaterEqual(packet1.crc, -256)
+        self.assertGreaterEqual(packet1.crc, 0)
         self.assertLessEqual(packet1.crc, 255)
 
-        self.assertGreaterEqual(packet1.header.crc, -256)
+        self.assertGreaterEqual(packet1.header.crc, 0)
         self.assertLessEqual(packet1.header.crc, 255)
+
+    def test_packet_w_str_buffer(self):
+        packet1 = xi.XIPacket(buffer="hello world")
+        payloadLength = len(packet1.payload)
+
+        self.assertEqual(len(packet1.payload), payloadLength)
+        self.assertEqual(packet1.header.payloadLength, payloadLength)
+        self.assertGreaterEqual(packet1.crc, 0)
+        self.assertLessEqual(packet1.crc, 255)
+
+        self.assertGreaterEqual(packet1.header.crc, 0)
+        self.assertLessEqual(packet1.header.crc, 255)
+
+    def test_create_header(self):
+        # tests the .rep property to ensure that
+        # a new XIPacket can be created by the string rep
+        # of an existing XIPacket.
+        packet1 = xi.XIPacket()
+        packet2 = xi.XIPacket.createXIPacket(packet1.rep)
+
+        self.assertEqual(packet1.rep, packet2.rep)
 
     def test_crc(self):
         payloadLength = 1000
-        packet1 = xi.XIPacket(buffer=[x % 0xFF for x in range(payloadLength)])
-        packet2 = xi.XIPacket(buffer=[x % 0xFF for x in range(payloadLength)])
+        packet1 = xi.XIPacket(
+            buffer="".join([chr(x % 0xFF) for x in range(payloadLength)]))
+        packet2 = xi.XIPacket(
+            buffer="".join([chr(x % 0xFF) for x in range(payloadLength)]))
 
-        self.assertEqual(packet1.header.payloadLength, packet2.header.payloadLength)
+        self.assertEqual(packet1.header.payloadLength,
+                         packet2.header.payloadLength)
         self.assertEqual(packet1.header.crc, packet2.header.crc)
         self.assertEqual(packet1.crc, packet2.crc)
 
+    def test_packet_creation(self):
+        payloadLength = xi.XIPacket.maxPayload()
+        packet1 = xi.XIPacket(
+            buffer="".join([chr(x % 0xFF) for x in range(payloadLength)]))
+        packet2 = xi.XIPacket.createXIPacket(packet1.rep)
 
-    def test_packet_w_invalid_buffer(self):
-        payloadLength = 1000
+        self.assertEqual(packet1.rep, packet2.rep)
 
-        # buffer must contain an array of bytes, therefore values must be in range(256)
-        with self.assertRaises(ValueError):
-            xi.XIPacket(buffer=[x for x in range(payloadLength)])
+        packet1 = xi.XIPacket(buffer="Hello World")
+        packet2 = xi.XIPacket.createXIPacket(packet1.rep)
 
+        self.assertEqual(packet1.rep, packet2.rep)
+
+    def test_bad_packet_creation(self):
+        payloadLength = xi.XIPacket.maxPayload()
+        packet1 = xi.XIPacket(
+            buffer="".join([chr(x % 0xFF) for x in range(payloadLength)]))
+
+        # act as though the message was interrupted
+        packet2 = xi.XIPacket.createXIPacket(packet1.rep[0:payloadLength / 2])
+
+        self.assertIsNone(packet2)
+
+    def test_non_default_values(self):
+        seqNumBits = xi.XIPacketHeader()._seq_num_bit_length
+
+        # test all possible type, src, dst, seq num combinations
+        for _type in [
+                xi.PACKET_TYPE.ACK, xi.PACKET_TYPE.CONNECTION,
+                xi.PACKET_TYPE.DATA
+        ]:
+            for seqNum in range(2**seqNumBits):
+                payloadLength = xi.XIPacket.maxPayload()
+                packet1 = xi.XIPacket(
+                    type=_type,
+                    seqNum=seqNum,
+                    buffer="".join(
+                        [chr(x % 0xFF) for x in range(payloadLength)]))
+                packet2 = xi.XIPacket.createXIPacket(packet1.rep)
+
+                self.assertEqual(packet1.rep, packet2.rep)
 
 
 if __name__ == '__main__':
