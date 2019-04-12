f24a2ca2a29ee049b819235526040832484a5e43
Venkat Garapati <venkat_garapati@outlook.com>
2019-04-12 16:35:50 -0400

    Only Show Plots if '--graphs' Arg is Passed
    Only shows plots instead of software tools if --graphs argument is
    passed. However, the PlutoSdr should be configured prior to doing this
    and should be noted that user will not know if an actual pluto is being
    shown or a sim.

--- start.py
+++ start.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     import logging
     import os
     import multiprocessing
-    
+    import argparse
+
     multiprocessing.freeze_support()
 
     filename = 'pluto.log'
@@ -16,13 +17,19 @@ if __name__ == "__main__":
 
     # logging.basicConfig(filename=filename, level=logging.DEBUG)
 
+    parser = argparse.ArgumentParser(
+        description="Team XI Capstone Software Tool")
+    parser.add_argument(
+        '--graphs', dest='show_graphs', action='store_true')
+
+    args = parser.parse_args()
+
     PlutoController.readRX = True
-    PlutoController.rx_show_all_plots = False
+    PlutoController.rx_show_all_plots = args.show_graphs
     PlutoController.threadPeriod = 1
 
     if PlutoController.rx_show_all_plots:
         PlutoController.getSdr()
-
 
     if not PlutoController.rx_show_all_plots:
         deviceInfoPage.vp_start_gui()
